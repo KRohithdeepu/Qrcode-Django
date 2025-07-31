8a8390ef717ba0c79e479652b24fe5af999e7d70
Karamala Rohith <karamalarohith012@gmail.com>
2025-07-31 22:20:14 +0530

    Changes

--- Django_Qr/settings.py
+++ Django_Qr/settings.py
@@ -24,10 +24,8 @@ SECRET_KEY = 'django-insecure-l9*j!mv5d=c_0f#x^jf1bb_-!o4awf06oj*elkxb1zsc(i60h7
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+ALLOWED_HOSTS = ['qrcode-django-6.onrender.com']
 
-ALLOWED_HOSTS = [
-    '*'
-]
 
 
 
@@ -44,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
